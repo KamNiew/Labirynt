7c23a3720d5034563fff7ba7bc0e175b9854a13a
KamNiew <80323084+KamNiew@users.noreply.github.com>
2021-03-20 17:45:12 +0100

    Add files via upload

---
+++ src/KluczZ.java
@@ -0,0 +1,18 @@
+public class KluczZ extends spraity {
+
+    public KluczZ(int x, int y) {
+        super(x, y);
+
+
+        loadImage("resoures/spraity/klucz_zloty.png");
+        getImageDimensions();
+    }
+
+
+    @Override
+    public String toString() {
+        return "Klucz{" +
+                "visible=" + visible +
+                '}';
+    }
+}
---
+++ src/wyjscie.java
@@ -0,0 +1,10 @@
+public class wyjscie extends spraity {
+    public wyjscie(int x, int y) {
+        super(x, y);
+
+
+        loadImage("resoures/spraity/wyjscie.png");
+
+        getImageDimensions();
+    }
+}
